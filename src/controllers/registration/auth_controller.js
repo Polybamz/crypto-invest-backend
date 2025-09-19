f51731faeac2342f6b06e5caab6414cf6d85a75b
Polybamz <polycarpnformi23262>
2025-09-19 02:33:33 +0100

    take a loan api

--- src/controllers/registration/auth_controller.js
+++ src/controllers/registration/auth_controller.js
@@ -160,4 +160,22 @@ export class AuthController {
         .json({ message: "Internal server error", error: error.message });
     }
   }
+
+  static async getUserById(req, res) {
+    const { uid } = req.params;
+    try {
+      const user = await AuthServices.getUserById(uid);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      return res
+        .status(200)
+        .json({ message: "User fetched successfully", data: user });
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ message: "Internal server error", error: error.message });
+    }
+  }
 }
