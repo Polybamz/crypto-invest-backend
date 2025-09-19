f3d9126c3b334716a9b850a24f90cb6340502057
Polybamz <polycarpnformi23262>
2025-09-19 02:46:47 +0100

    take a loan api

--- src/services/registration/auth_service.js
+++ src/services/registration/auth_service.js
@@ -1,15 +1,15 @@
 import { db, admin } from '../../config/config.js';
-import {signInWithEmailAndPassword} from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 class AuthServices {
     static async registerUser(email, password, firstName, lastName, referralCode, referredBy) {
         try {
             console.log(email, password, firstName, lastName, referralCode, referredBy);
             const user = await admin.auth().createUser({
-                    email: email,
-                    password: password,
-                    displayName: `${firstName} ${lastName}`,
-                });
+                email: email,
+                password: password,
+                displayName: `${firstName} ${lastName}`,
+            });
             /// Add user to firestore
             await db.collection('users').doc(user.uid).set({
                 email: email,
@@ -28,59 +28,59 @@ class AuthServices {
             throw new Error(error);
         }
     }
-static getUserById(uid) {
-    console.log('uid', uid);
-    try {
-        const userDoc = db.collection('users').doc(uid).get();
-        if (!userDoc.exists) {
-            console.log('No such user!');
+    static getUserById(uid) {
+        console.log('uid', uid);
+        try {
+            const userDoc = db.collection('users').doc(uid).get();
+            if (!userDoc.exists) {
+                console.log('No such user!');
+                return null;
+            }
+            console.log('userDoc.data()', userDoc.data());
+            return { uid: uid, ...userDoc.data() };
+        } catch (error) {
+            console.log(error);
             return null;
         }
-        console.log('userDoc.data()', userDoc.data());
-        return { uid: uid, ...userDoc.data() };
-    } catch (error) {
-        console.log(error);
-        return null;
+
     }
 
-}
-
-static getwalletBalance(uid) {  
-    try {
-        const userDoc = db.collection('users').doc(uid).get();
-        if (!userDoc.exists) {
-            console.log('No such user!');
+    static getwalletBalance(uid) {
+        try {
+            const userDoc = db.collection('users').doc(uid).get();
+            if (!userDoc.exists) {
+                console.log('No such user!');
+                return null;
+            }
+            console.log('userDoc.data()', userDoc.data());
+            return { uid: uid, balance: userDoc.data().walletBalance };
+        } catch (error) {
+            console.log(error);
             return null;
         }
-        console.log('userDoc.data()', userDoc.data());
-        return { uid: uid, balance: userDoc.data().walletBalance };
-    } catch (error) {
-        console.log(error);
-        return null;
     }
-}
 
- static async getUserByReferralCode(referralCode) {
+    static async getUserByReferralCode(referralCode) {
 
-    console.log('nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn',referralCode);
-    try {
-      const userSnap = await db
-        .collection('users')
-        .where('referralCode', '==', `${referralCode}`)
-      
-        .get();
-        console.log('userSnappppppppppppppppppppppppppppppppppppppp', userSnap);
-            
+        console.log('nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn', referralCode);
+        try {
+            const userSnap = await db
+                .collection('users')
+                .where('referralCode', '==', `${referralCode}`)
 
-      if (userSnap.empty) return null;
-      console.log('userSnap.docs[0].data()', userSnap.docs[0].data());
-      // return plain object with userId included
-      return { ...userSnap.docs[0].data(), id: userSnap.docs[0].id };
-    } catch (error) {
-      console.error(error);
-      return null;
+                .get();
+            console.log('userSnappppppppppppppppppppppppppppppppppppppp', userSnap);
+
+
+            if (userSnap.empty) return null;
+            console.log('userSnap.docs[0].data()', userSnap.docs[0].data());
+            // return plain object with userId included
+            return { ...userSnap.docs[0].data(), id: userSnap.docs[0].id };
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
     }
-  }
 
     static async loginUser(email, password) {
         try {
@@ -93,7 +93,7 @@ static getwalletBalance(uid) {
             return null;
         }
     }
-   // Fetch user from firestore by UID
+    // Fetch user from firestore by UID
     static async getUserById(uid) {
         try {
             const userDoc = await db.collection('users').doc(uid).get();
