69aefd4677f87d888ea3893c113b3d09af5a0e8b
Polybamz <polycarpnformi23262>
2025-09-19 19:36:42 +0100

    take a inment

--- src/routes/invesment/investment_route.js
+++ src/routes/invesment/investment_route.js
@@ -1,16 +1,16 @@
 import express from 'express';
-import { getInvestmentPlans, createInvestment, getUserInvestments, createPlan, getAllInvestments, updateInvestmentStatus, getInvestmentAnalytics, getUserInvestmentAnalytics } from '../../controllers/investment/investment_controller.js';
+import { getInvestmentPlans, createInvestment, getUserInvestments, createPlan, getAllInvestments, updateInvestmentStatus, getInvestmentAnalytics,  } from '../../controllers/investment/investment_controller.js';
 
 const router = express.Router();
 
 router.get('/plans', getInvestmentPlans);
-//router.post('/create-plans', createPlan);
+router.post('/create-plans', createPlan);
 router.post('/make-investment', createInvestment);
 router.get('/user-investments/:userId',  getUserInvestments);
 router.get('/all-investments',  getAllInvestments);
 router.put('/investment/:investmentId/:status',  updateInvestmentStatus);
 router.get('/investment-analysis', getInvestmentAnalytics)
-router.get('/user/:userId/investment-analysis', getUserInvestmentAnalytics)
+//router.get('/user/:userId/investment-analysis', getUserInvestmentAnalytics)
 
 
 export default router;
